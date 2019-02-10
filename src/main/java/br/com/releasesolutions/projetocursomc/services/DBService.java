0cee9c9a4663c5af88e18cb7ef532e9c58a7879f
Robson Sousa <robson.sousa@releasesolutions.com.br>
2019-02-10 07:29:46 -0200

    Acrescentando mais produtos para testar a funcionalidade de 'Infinity Scroll'.

--- src/main/java/br/com/releasesolutions/projetocursomc/services/DBService.java
+++ src/main/java/br/com/releasesolutions/projetocursomc/services/DBService.java
@@ -70,8 +70,54 @@ public class DBService {
         Produto produto9 = new Produto(null, "Abajour", 100.00);
         Produto produto10 = new Produto(null, "Pendente", 180.00);
         Produto produto11 = new Produto(null, "Shampoo", 90.00);
+        Produto produto12 = new Produto(null, "Produto 12", 2000.00);
+        Produto produto13 = new Produto(null, "Produto 13", 800.00);
+        Produto produto14 = new Produto(null, "Produto 14", 80.00);
+        Produto produto15 = new Produto(null, "Produto 15", 300.00);
+        Produto produto16 = new Produto(null, "Produto 16", 50.00);
+        Produto produto17 = new Produto(null, "Produto 17", 200.00);
+        Produto produto18 = new Produto(null, "Produto 18", 1200.00);
+        Produto produto19 = new Produto(null, "Produto 19", 800.00);
+        Produto produto20 = new Produto(null, "Produto 20", 100.00);
+        Produto produto21 = new Produto(null, "Produto 21", 180.00);
+        Produto produto22 = new Produto(null, "Produto 22", 2000.00);
+        Produto produto23 = new Produto(null, "Produto 23", 800.00);
+        Produto produto24 = new Produto(null, "Produto 24", 80.00);
+        Produto produto25 = new Produto(null, "Produto 25", 300.00);
+        Produto produto26 = new Produto(null, "Produto 26", 50.00);
+        Produto produto27 = new Produto(null, "Produto 27", 200.00);
+        Produto produto28 = new Produto(null, "Produto 28", 1200.00);
+        Produto produto29 = new Produto(null, "Produto 29", 800.00);
+        Produto produto30 = new Produto(null, "Produto 30", 100.00);
+        Produto produto31 = new Produto(null, "Produto 31", 180.00);
+        Produto produto32 = new Produto(null, "Produto 32", 2000.00);
+        Produto produto33 = new Produto(null, "Produto 33", 800.00);
+        Produto produto34 = new Produto(null, "Produto 34", 80.00);
+        Produto produto35 = new Produto(null, "Produto 35", 300.00);
+        Produto produto36 = new Produto(null, "Produto 36", 50.00);
+        Produto produto37 = new Produto(null, "Produto 37", 200.00);
+        Produto produto38 = new Produto(null, "Produto 38", 1200.00);
+        Produto produto39 = new Produto(null, "Produto 39", 800.00);
+        Produto produto40 = new Produto(null, "Produto 40", 100.00);
+        Produto produto41 = new Produto(null, "Produto 41", 180.00);
+        Produto produto42 = new Produto(null, "Produto 42", 2000.00);
+        Produto produto43 = new Produto(null, "Produto 43", 800.00);
+        Produto produto44 = new Produto(null, "Produto 44", 80.00);
+        Produto produto45 = new Produto(null, "Produto 45", 300.00);
+        Produto produto46 = new Produto(null, "Produto 46", 50.00);
+        Produto produto47 = new Produto(null, "Produto 47", 200.00);
+        Produto produto48 = new Produto(null, "Produto 48", 1200.00);
+        Produto produto49 = new Produto(null, "Produto 49", 800.00);
+        Produto produto50 = new Produto(null, "Produto 50", 100.00);
 
         categoria1.getProdutos().addAll(Arrays.asList(produto1, produto2, produto3));
+
+        categoria1.getProdutos().addAll(Arrays.asList(produto12, produto13, produto14, produto15, produto16, produto17, produto18, produto19, produto20, produto21,
+                produto22, produto23, produto24, produto25, produto26, produto27, produto28, produto29, produto30, produto31, produto32, produto33, produto34, produto35,
+                produto36, produto37, produto38, produto39, produto40, produto31, produto32, produto33, produto34, produto35, produto36, produto37, produto38, produto39,
+                produto40, produto41, produto42, produto43, produto44, produto45, produto46, produto47, produto48, produto49, produto50));
+
+
         categoria2.getProdutos().addAll(Arrays.asList(produto2, produto4));
         categoria3.getProdutos().addAll(Arrays.asList(produto5, produto6));
         categoria4.getProdutos().addAll(Arrays.asList(produto1, produto2, produto3, produto7));
@@ -91,8 +137,53 @@ public class DBService {
         produto10.getCategorias().add(categoria6);
         produto11.getCategorias().add(categoria7);
 
+        produto12.getCategorias().add(categoria1);
+        produto13.getCategorias().add(categoria1);
+        produto14.getCategorias().add(categoria1);
+        produto15.getCategorias().add(categoria1);
+        produto16.getCategorias().add(categoria1);
+        produto17.getCategorias().add(categoria1);
+        produto18.getCategorias().add(categoria1);
+        produto19.getCategorias().add(categoria1);
+        produto20.getCategorias().add(categoria1);
+        produto21.getCategorias().add(categoria1);
+        produto22.getCategorias().add(categoria1);
+        produto23.getCategorias().add(categoria1);
+        produto24.getCategorias().add(categoria1);
+        produto25.getCategorias().add(categoria1);
+        produto26.getCategorias().add(categoria1);
+        produto27.getCategorias().add(categoria1);
+        produto28.getCategorias().add(categoria1);
+        produto29.getCategorias().add(categoria1);
+        produto30.getCategorias().add(categoria1);
+        produto31.getCategorias().add(categoria1);
+        produto32.getCategorias().add(categoria1);
+        produto33.getCategorias().add(categoria1);
+        produto34.getCategorias().add(categoria1);
+        produto35.getCategorias().add(categoria1);
+        produto36.getCategorias().add(categoria1);
+        produto37.getCategorias().add(categoria1);
+        produto38.getCategorias().add(categoria1);
+        produto39.getCategorias().add(categoria1);
+        produto40.getCategorias().add(categoria1);
+        produto41.getCategorias().add(categoria1);
+        produto42.getCategorias().add(categoria1);
+        produto43.getCategorias().add(categoria1);
+        produto44.getCategorias().add(categoria1);
+        produto45.getCategorias().add(categoria1);
+        produto46.getCategorias().add(categoria1);
+        produto47.getCategorias().add(categoria1);
+        produto48.getCategorias().add(categoria1);
+        produto49.getCategorias().add(categoria1);
+        produto50.getCategorias().add(categoria1);
+
         categoriaRepository.saveAll(Arrays.asList(categoria1, categoria2, categoria3, categoria4, categoria5, categoria6, categoria7));
         produtoRepository.saveAll(Arrays.asList(produto1, produto2, produto3, produto4, produto5, produto6, produto7, produto8, produto9, produto10, produto11));
+
+        produtoRepository.saveAll(Arrays.asList(produto12, produto13, produto14, produto15, produto16, produto17, produto18, produto19, produto20,
+                produto21, produto22, produto23, produto24, produto25, produto26, produto27, produto28, produto29, produto30,
+                produto31, produto32, produto33, produto34, produto35, produto36, produto37, produto38, produto39, produto40,
+                produto41, produto42, produto43, produto44, produto45, produto46, produto47, produto48, produto49, produto50));
 
         Estado estado1 = new Estado(null, "São Paulo");
         Estado estado2 = new Estado(null, "Rio de Janeiro");
