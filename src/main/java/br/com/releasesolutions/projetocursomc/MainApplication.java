b6a3cd52b2cbff76ba02eab219ee46586212581d
Robson Sousa <robsoncartes@outlook.com>
2023-01-23 22:52:55 -0300

    refactor: remoção do método 'run' e da implementação da interface 'CommandLineRunner' na classe 'MainApplication'.

--- src/main/java/br/com/releasesolutions/projetocursomc/MainApplication.java
+++ src/main/java/br/com/releasesolutions/projetocursomc/MainApplication.java
@@ -1,19 +1,13 @@
 package br.com.releasesolutions.projetocursomc;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MainApplication implements CommandLineRunner {
+public class MainApplication {
 
     public static void main(String[] args) {
 
         SpringApplication.run(MainApplication.class, args);
-    }
-
-    @Override
-    public void run(String... args) {
-
     }
 }
